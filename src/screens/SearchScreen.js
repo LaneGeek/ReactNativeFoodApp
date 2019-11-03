210e184744edd1a70b60da3d5533d413c4679a20
HS <sekhah@my.lanecc.edu>
2019-11-03 15:08:30 -0800

    Finished App and Section 11.

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,22 +1,29 @@
-import React, {useState, useEffect} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, Text, View, ScrollView} from 'react-native';
 import SearchBar from '../components/SearchBar';
 import useResults from '../hooks/useResults';
+import ResultsList from '../components/ResultsList';
 
 const SearchScreen = () => {
     const [term, setTerm] = useState('');
     const [searchApi, results, errorMessage] = useResults();
 
+    const filterResultsByPrice = price => results.filter(x => x.price === price);
+
     return (
-        <View>
+        <>
             <SearchBar
                 term={term}
                 onTermChange={setTerm}
                 onTermSubmit={() => searchApi(term)}
             />
             {errorMessage ? <Text>{errorMessage}</Text> : null}
-            <Text>We have found {results.length} results</Text>
-        </View>
+            <ScrollView>
+                <ResultsList results={filterResultsByPrice('$')} title="Cost Effective"/>
+                <ResultsList results={filterResultsByPrice('$$')} title="Bit Pricier"/>
+                <ResultsList results={filterResultsByPrice('$$$')} title="Big Spender"/>
+            </ScrollView>
+        </>
     );
 };
 
